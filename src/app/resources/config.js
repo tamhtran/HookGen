a480af41bb785ebb88e27ce9a5a2e1b618a118ce
ttran <ms.tam.h.tran@gmail.com>
2025-04-25 13:42:46 -0700

    Initial commit

---
+++ src/app/resources/config.js
@@ -0,0 +1,114 @@
+// IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
+const baseURL = "https://demo.once-ui.com";
+
+// Import and set font for each variant
+import { Geist } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
+
+const primaryFont = Geist({
+  variable: "--font-primary",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const monoFont = Geist_Mono({
+  variable: "--font-code",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const font = {
+  primary: primaryFont,
+  secondary: primaryFont,
+  tertiary: primaryFont,
+  code: monoFont,
+};
+
+// default customization applied to the HTML in the main layout.tsx
+const style = {
+  theme: "dark", // dark | light - not needed when using ThemeProvider
+  neutral: "gray", // sand | gray | slate
+  brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast | inverse
+  solidStyle: "flat", // flat | plastic
+  border: "playful", // rounded | playful | conservative
+  surface: "filled", // filled | translucent
+  transition: "all", // all | micro | macro
+  scaling: "100", // 90 | 95 | 100 | 105 | 110
+};
+
+const effects = {
+  mask: {
+    cursor: false,
+    x: 100,
+    y: 0,
+    radius: 100,
+  },
+  gradient: {
+    display: true,
+    opacity: 90,
+    x: 100,
+    y: 60,
+    width: 70,
+    height: 50,
+    tilt: -40,
+    colorStart: "accent-background-strong",
+    colorEnd: "page-background",
+  },
+  dots: {
+    display: true,
+    opacity: 20,
+    size: "2",
+    color: "brand-on-background-weak",
+  },
+  grid: {
+    display: true,
+    opacity: 100,
+    color: "accent-alpha-weak",
+    width: "0.25rem",
+    height: "0.25rem",
+  },
+  lines: {
+    display: false,
+    opacity: 100,
+    color: "neutral-alpha-weak",
+    size: "16",
+    thickness: 1,
+    angle: 45,
+  },
+};
+
+// metadata for pages
+const meta = {
+  home: {
+    path: "/",
+    title: "Once UI for Next.js",
+    description: "An open-source design system and component library for Next.js that emphasizes easy styling and accessibility in UI development.",
+    image: "/og/home.jpg",
+    canonical: "https://once-ui.com",
+    robots: "index,follow",
+    alternates: [
+      { href: "https://once-ui.com", hrefLang: "en" },
+    ],
+  },
+  // add more routes and reference them in page.tsx
+};
+
+// default schema data
+const schema = {
+  logo: "",
+  type: "Organization",
+  name: "Once UI",
+  description: meta.home.description,
+  email: "lorant@once-ui.com",
+};
+
+// social links
+const social = {
+  twitter: "https://www.twitter.com/_onceui",
+  linkedin: "https://www.linkedin.com/company/once-ui/",
+  discord: "https://discord.com/invite/5EyAQ4eNdS",
+};
+
+export { baseURL, font, style, meta, schema, social, effects };
